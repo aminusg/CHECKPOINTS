e8f8cd30d8002912b1899e909d43d6db7d8c3d13
ainhoa <aino20m@gmail.com>
2024-05-13 21:26:37 +0200

    solución

--- checkpoint7.js
+++ checkpoint7.js
@@ -4,8 +4,11 @@ function myCalculator (num1, num2, num3, num4) {
     product = sum1 * sum2;
     if (product > 50) {
         return "¡El número es mayor que 50!";
-    } else {
+    } else if (product < 50) {
         return "¡El número es menor que 50!";
+    } else {
+        return "Solo puede ser 50"
     }
 };
 console.log(myCalculator(4,13,22,3));
+console.log(myCalculator(1,2,3,5));
